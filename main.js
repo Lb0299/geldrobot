0012e9e9e839c1f6d48fb84c7989d82fc8207dac
Lb0299 <Lester.berghammer@gmail.com>
2025-06-14 15:00:38 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('LB’s Geldrobot actief');
+function switchScreen(id) {
+  const screens = document.querySelectorAll(".screen");
+  screens.forEach((s) => s.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
+}
+
+console.log("LB’s Geldrobot uitgebreid geladen");
